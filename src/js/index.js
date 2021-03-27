0aaeaeb398aea44ccef7a3eca8a33d8ba4345fe2
pjy1368 <basejin0810@naver.com>
2021-03-27 22:57:06 +0900

    feat: User의 투두리스트를 불러오고, 그에 맞춰 제목을 변경하는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -22,6 +22,7 @@ const onUserCreateHandler = () => {
     .then(data => {
       const buttonElement = document.createElement('button');
       buttonElement.className = "ripple";
+      buttonElement.id = data._id;
       buttonElement.innerText = data.name;
       userList.insertBefore(buttonElement, userCreateButton);
     })
@@ -34,3 +35,40 @@ const onUserCreateHandler = () => {
 const userCreateButton = document.querySelector('.user-create-button');
 userCreateButton.addEventListener('click', onUserCreateHandler);
 const userList = document.getElementById("user-list");
+userList.addEventListener('click', onUserSelectHandler);
+
+function loadUserList() {
+  fetch("https://js-todo-list-9ca3a.df.r.appspot.com/api/users")
+    .then(data => {
+      if (!data.ok) {
+        throw new Error(data.status)
+      }
+      return data.json()
+    })
+    .then(data => {
+      for (let i = 0; i < data.length; i++) {
+        const buttonElement = document.createElement('button');
+        buttonElement.className = "ripple";
+        buttonElement.id = data[i]._id;
+        buttonElement.innerText = data[i].name;
+        userList.insertBefore(buttonElement, userList.children[1]);
+      }
+    });
+}
+loadUserList();
+
+function onUserSelectHandler(event) {
+  const item = event.target.closest('button');
+  if (item === null || (!item.classList.contains("basic") && item.id === "")) {
+    return;
+  }
+
+  for (let i = 0; i < userList.children.length; i++) {
+    if (userList.children[i].classList.contains("active")) {
+      userList.children[i].classList.toggle("active");
+    }
+  }
+
+  item.classList.toggle("active");
+  document.querySelector(".user-name").innerText = item.innerText;
+}
