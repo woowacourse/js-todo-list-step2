a0ecf93c210c562f0223d3160a86d0c842a8bcbf
pjy1368 <basejin0810@naver.com>
2021-03-27 18:14:23 +0900

    feat(index): User 추가하는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,36 @@
 const onUserCreateHandler = () => {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
-}
+  const newUser = {
+    name: userName
+  };
 
-const userCreateButton = document.querySelector('.user-create-button')
-userCreateButton.addEventListener('click', onUserCreateHandler)
+  const option = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUser)
+  };
 
+  fetch("https://js-todo-list-9ca3a.df.r.appspot.com/api/users", option)
+    .then(data => {
+      if (!data.ok) {
+        throw new Error(data.status)
+      }
+      return data.json()
+    })
+    .then(data => {
+      const buttonElement = document.createElement('button');
+      buttonElement.className = "ripple";
+      buttonElement.innerText = data.name;
+      userList.insertBefore(buttonElement, userCreateButton);
+    })
+    .catch(error => {
+      console.log(error)
+    });
+
+};
+
+const userCreateButton = document.querySelector('.user-create-button');
+userCreateButton.addEventListener('click', onUserCreateHandler);
+const userList = document.getElementById("user-list");
