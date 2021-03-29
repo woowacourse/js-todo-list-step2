9fcbebd04ce6b676bd4feec8349c206d0b3b650b
Junroot <junroot0909@gmail.com>
2021-03-29 21:59:10 +0900

    feat: todoItem 추가하기 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,99 @@
 import * as userList from "./userList.js"
+import * as todoList from "./todoList.js"
 
-const onUserCreateHandler = () => {
-  userList.addUser();
+const userNameCountMin = 2;
+const baseApiUrl = "https://js-todo-list-9ca3a.df.r.appspot.com/api/users";
+let users = [];
+let selectedUserId = "";
+
+const loadData = async () => {
+  const response = await fetch(baseApiUrl);
+  users = await response.json();
+
+  if (selectedUserId === "") {
+    selectedUserId = users[0]["_id"];
+  }
+}
+
+const getTodosById = (id) => {
+  return users.find(user => user["_id"] === id)["todoList"];
+}
+
+const isValidName = (name) => {
+  if (name === null) {
+    return false;
+  }
+  if (name.length < userNameCountMin) {
+    alert(userNameCountMin + "자 이상의 이름을 입력해주세요.");
+    return false;
+  }
+  return true;
+}
+
+const onUserCreateHandler = async () => {
+  const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  if (isValidName(userName)) {
+    const response = await fetch(baseApiUrl, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({name: userName})
+    });
+    const newUser = await response.json();
+    users.push(newUser);
+    userList.updateUserList(users, selectedUserId);
+  }
 }
 
 function onUserDeleteHandler() {
-  userList.deleteUser();
+  const idToRemove = selectedUserId;
+  users = users.filter(user => user["_id"] !== selectedUserId);
+  selectedUserId = users[0]["_id"];
+  userList.updateUserList(users, selectedUserId);
+  todoList.updateTodoList(getTodosById(selectedUserId));
+  fetch(baseApiUrl + "/" + idToRemove, {
+    method: "DELETE"
+  }).then();
 }
 
 const onSelectUserHandler = (event) => {
   if (event.target && event.target.tagName === "BUTTON") {
-    userList.updateTodoList(event.target.id);
+    selectedUserId = event.target.id;
+    userList.updateUserList(users, selectedUserId);
+    todoList.updateTodoList(getTodosById(selectedUserId));
+  }
+}
+
+const onInputNewTodoHandler = async (event) => {
+  if (event.key === "Enter" && event.target.value) {
+    if (event.target.value.length < userNameCountMin) {
+      alert("내용은 2자 이상이어야 합니다.");
+      return;
+    }
+    const response = await fetch(baseApiUrl+"/"+selectedUserId+"/items", {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({"contents": event.target.value})
+    })
+    const result = await response.json();
+    event.target.value = "";
+    getTodosById(selectedUserId).push(result);
+    todoList.updateTodoList(getTodosById(selectedUserId));
   }
 }
 
 document.querySelector('.user-create-button').addEventListener('click', onUserCreateHandler)
-
 document.querySelector(".user-delete-button").addEventListener('click', onUserDeleteHandler)
 document.querySelector(".users").addEventListener('click', onSelectUserHandler);
+document.querySelector(".new-todo").addEventListener("keyup", onInputNewTodoHandler);
+
 
 window.onload = () => {
-  userList.loadData().then(
-      userList.updateUserList
-  );
+  loadData().then(() => {
+      userList.updateUserList(users, selectedUserId);
+      todoList.updateTodoList(getTodosById(selectedUserId));
+  });
 }
--- src/js/userList.js
+++ src/js/userList.js
@@ -1,71 +1,10 @@
-import * as todoList from "./todoList.js";
 
-const userNameCountMin = 2;
 
-const baseApiUrl = "https://js-todo-list-9ca3a.df.r.appspot.com/api/users";
 const userButtonTemplate = (name, id) => `<button class="ripple" id=${id}>${name}</button>`;
-let selectedUserId = "";
 
-let users = [];
 const $userList = document.querySelector(".users");
 
-export const loadData = async () => {
-  const response = await fetch(baseApiUrl);
-  const result = await response.json()
-
-  users = result;
-  if (selectedUserId === "") {
-    selectedUserId = users[0]["_id"];
-  }
-}
-
-export const updateTodoList = (id) => {
-  selectedUserId = id;
-  document.querySelector("button.active").classList.remove("active");
-  document.getElementById(id).classList.add("active");
-  todoList.updateTodoList(users.find(user => user["_id"] === id)["todoList"]);
-}
-
-export const updateUserList = () => {
+export const updateUserList = (users, selectedUserId) => {
   $userList.innerHTML = users.map(user => userButtonTemplate(user["name"], user["_id"])).join("");
   document.getElementById(selectedUserId).classList.add("active");
-}
-
-const isValidName = (name) => {
-  if (name === null) {
-    return false;
-  }
-  if (name.length < userNameCountMin) {
-    alert(userNameCountMin + "자 이상의 이름을 입력해주세요.");
-    return false;
-  }
-  return true;
-}
-
-export const addUser = () => {
-  const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
-  if (isValidName(userName)) {
-    fetch(baseApiUrl, {
-      headers: {
-        "Content-Type": "application/json"
-      },
-      method: "POST",
-      body: JSON.stringify({name: userName})
-    }).then(() => {
-      loadData().then(updateUserList)
-    });
-  }
-}
-
-export const deleteUser = () => {
-  fetch(baseApiUrl + "/" + selectedUserId, {
-    method: "DELETE"
-  }).then(() => {
-    selectedUserId = "";
-    loadData().then(() => {
-      updateUserList();
-      console.log(selectedUserId);
-      updateTodoList(selectedUserId);
-    });
-  });
 }
