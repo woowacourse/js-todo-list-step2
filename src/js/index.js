e04302fc292f3ffb462438b4d9ea0f18ef1e981a
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-29 23:43:44 +0900

    feat : 백엔드와 통신할 Service.js 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import * as service from './service.js';
+
 const onUserCreateHandler = () => {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
 }
@@ -5,3 +7,5 @@ const onUserCreateHandler = () => {
 const userCreateButton = document.querySelector('.user-create-button')
 userCreateButton.addEventListener('click', onUserCreateHandler)
 
+console.log("테스트");
+console.log(await service.getUsers());
