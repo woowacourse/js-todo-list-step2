3ca1ed980f2e66577ab8e451493d4d0f5e7ad411
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-30 16:10:11 +0900

    feat : User를 클릭했을 때 새 TodoList를 렌더링 시키는 기능 추가

--- src/js/component/userList/UserComponent.js
+++ src/js/component/userList/UserComponent.js
@@ -1,14 +1,21 @@
+import * as TodoList from './TodoListComponent.js';
+
 export const createUserElement = (textContent, _id) => {
     const element = document.createElement("button");
     element.className = "ripple";
     element.textContent = textContent;
     element.setAttribute("data-id", _id);
-    element.addEventListener("click", clickUserAndSetActive)
+    element.addEventListener("click", onClickUser)
     return element;
 }
 
-const clickUserAndSetActive = (event) => {
+const onClickUser = (event) => {
     resetUserToNotActive();
+    clickedUserSetActive(event);
+    TodoList.getTodoListAndRender(event.target.getAttribute("data-id"));
+}
+
+const clickedUserSetActive = (event) => {
     const $clickedUser = event.target;
     $clickedUser.classList.add("active");
 }
