58dee5f50e58f928496703c610094a4d3451b9f1
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-30 19:44:19 +0900

    feat : 더블 클릭했을 때 수정 상태로 바꾸기

--- src/js/component/userList/TodoItemComponent.js
+++ src/js/component/userList/TodoItemComponent.js
@@ -3,10 +3,11 @@ import * as service from '../../service.js';
 export const addEvent = () => {
     const $todoList = document.querySelector(".todo-list");
     $todoList.addEventListener("click", clickTodoItem);
+    $todoList.addEventListener("dblclick", dblclickTodoItem);
 }
 
 const clickTodoItem = async (event) => {
-    if (event.target && event.target.nodeName == "INPUT") {
+    if (event.target && event.target.nodeName === "INPUT") {
         toggleTodoItemToBeCompleted(event);
         return;
     }
@@ -30,4 +31,30 @@ const deleteTodoItem = async (event) => {
     const userId = $activeUser.dataset.id;
     $todoItem.remove();
     service.deleteTodoItem(userId, $todoItem.dataset.id)
+}
+
+const dblclickTodoItem = (event) => {
+    if (event.target && event.target.nodeName === "LABEL") {
+        convertToEditStatus(event);
+    }
+}
+
+const convertToEditStatus = (event) => {
+    const $todoItem = event.target.closest("li");
+    $todoItem.classList.toggle("editing");
+    const $input = $todoItem.querySelector(".edit");
+    $input.value = getTextFromTodoItem($todoItem);
+}
+
+const getTextFromTodoItem = ($todoItem) => {
+    const cloneLabel = $todoItem.querySelector(".label").cloneNode(true);
+    const selectOption = cloneLabel.querySelector("select");
+    const chip = cloneLabel.querySelector(".chip");
+    if (selectOption) {
+        selectOption.remove();
+    }
+    if (chip) {
+        chip.remove();
+    }
+    return cloneLabel.textContent.trim();
 }
