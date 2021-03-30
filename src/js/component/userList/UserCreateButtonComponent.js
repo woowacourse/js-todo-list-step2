e8eb8debcbdc957b943d3a4c8132e7c1d54bebaf
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-30 20:31:55 +0900

    feat : User의 이름은 최소 2글자 이상이어야 하는 검증 추가하기

--- src/js/component/userList/UserCreateButtonComponent.js
+++ src/js/component/userList/UserCreateButtonComponent.js
@@ -8,14 +8,18 @@ export function addEvent() {
 
 const onUserCreateHandler = async () => {
     const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+    if (userName.length < 2) {
+        alert("2글자 이상이어야 합니다.");
+        return;
+    }
     const createUserApi = await service.createUser(userName);
     const userId = createUserApi.data._id;
     addUser(userName, userId);
 }
 
 const addUser = (userName, userId) => {
-    const $userList = document.querySelector('#user-list')
-    const $userCreateButton = document.querySelector('.user-create-button')
+    const $userList = document.querySelector('#user-list');
+    const $userCreateButton = document.querySelector('.user-create-button');
     const userButton = user.createUserElement(userName, userId);
     $userList.insertBefore(userButton, $userCreateButton);
 }
