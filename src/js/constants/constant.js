7b067d35e67b22d8769f5e7fbbcd1a7a7f50e5cb
Minsung Son <sns2831@gmail.com>
2021-03-31 15:20:30 +0900

    feat: 상수 정의

---
+++ src/js/constants/constant.js
@@ -0,0 +1,28 @@
+export const SELECTOR = {
+    USER_TITLE: "#user-title",
+    USER_LIST: "#user-list",
+    USER_CREATE_BUTTON: ".user-create-button",
+    USER_DELETE_BUTTON: ".user-delete-button",
+
+    NEW_TODO: ".new-todo",
+    TODO_LIST: ".todo-list",
+    CHIP: ".chip",
+    DESTROY: ".destroy",
+    EDIT: ".edit",
+
+    TODO_COUNT: ".todo-count",
+    FILTERS: ".filters",
+    CLEAR_COMPLETED: ".clear-completed"
+}
+
+export const CLASS = {
+    SELECTED: "selected",
+    PRIMARY: "primary",
+    SECONDARY: "secondary",
+
+}
+
+export const URL = {
+    BASE: "https://js-todo-list-9ca3a.df.r.appspot.com"
+}
+
