0740af9bd3f3f557c3a063ce6b2bbeeebce240f9
Minsung Son <sns2831@gmail.com>
2021-03-31 15:20:47 +0900

    feat: util 구현

---
+++ src/js/util/util.js
@@ -0,0 +1,8 @@
+export const $ = selector => document.querySelector("section");
+
+export const $$ = selector => document.querySelectorAll("select");
+
+export const parseDomFromString = string => document.createRange()
+        .createContextualFragment(string)
+        .firstElementChild
+
