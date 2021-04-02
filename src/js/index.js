077872bb66ddd00bc3fdd7d9d929fe270a93cc1e
kimjunseo <air.junseo@gmail.com>
2021-04-02 20:27:59 +0900

    feat : 저장된 유저 보여주기

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,49 @@
 const BASE_URL = 'https://js-todo-list-9ca3a.df.r.appspot.com';
 
+function showAllUsersForm() {
+    return {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+}
+
+async function showAllUsers(userId) {
+    const showAllUsersResponse = await fetch(BASE_URL + "/api/users", showAllUsersForm());
+    const allUsers = await showAllUsersResponse.json();
+    const userList = document.getElementById("user-list");
+    for (let i = 0; i < allUsers.length; i++) {
+        if (document.getElementById(allUsers[i]._id) == null) {
+            const userButton = document.createElement("button");
+            userButton.id = allUsers[i]._id;
+            userButton.innerText = allUsers[i].name;
+            userButton.classList.add("ripple");
+            userList.prepend(userButton);
+        }
+    }
+    showActiveUser(userId, userList);
+}
+
+function showActiveUser(userId, userList) {
+    const activeUser = document.getElementById(userId);
+    if (activeUser == null) {
+        userList.firstChild.classList.add("active");
+    } else {
+        const activeUsers = document.getElementsByClassName("active");
+        for (let i = 0; i < activeUsers.length; i++) {
+            activeUsers[i].classList.remove("active");
+        }
+        activeUser.classList.add("active");
+    }
+}
+
 async function userCreateHandler() {
     const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
     if (userName.length >= 2) {
         const userForm = createUserForm(userName);
-        const createdUserName = await createUser(userForm);
-        console.log(createdUserName);
+        const createdUserId = await createUser(userForm);
+        await showAllUsers(createdUserId);
     } else {
         alert("이름은 두 글자 이상이어야 합니다!");
     }
@@ -14,7 +52,7 @@ async function userCreateHandler() {
 async function createUser(userForm) {
     const createdUserResponse = await fetch(BASE_URL + "/api/users", userForm);
     const createdUser = await createdUserResponse.json();
-    return createdUser.name;
+    return createdUser._id;
 }
 
 function createUserForm(userName) {
@@ -31,6 +69,7 @@ function createUserForm(userName) {
     };
 }
 
+showAllUsers(0);
 const userCreateButton = document.querySelector('.user-create-button')
 userCreateButton.addEventListener('click', userCreateHandler)
 
