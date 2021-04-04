778f7f61d8385cb93909141d47aa5834a4564b3a
hyeon9mak <jinha3507@gmail.com>
2021-04-04 19:57:15 +0900

    refactor: fix append created user on user list

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,13 @@ import * as form from "./requestForm.js";
 import * as uri from "./requestUri.js";
 import * as template from "./template.js";
 
+const $userList = document.querySelector("#user-list");
+
 window.onload = onLoadUsers;
 
 async function onLoadUsers() {
-    const $userList = document.querySelector("#user-list")
     const responseUsers = await fetch(uri.users, form.get());
     const users = await responseUsers.json();
-
-    while($userList.children.length > 2) {
-        $userList.removeChild($userList.firstChild);
-    }
 
     for (const user of users) {
         const userDom = createElementByString(template.user(user._id, user.name));
@@ -21,14 +18,14 @@ async function onLoadUsers() {
 
 const onUserCreateHandler = async () => {
     const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
-
     if (userName.length < 2) {
         window.alert("2글자 이상이어야 합니다.");
         return;
     }
 
-    await fetch(uri.users, form.postCreateUser(userName));
-    await onLoadUsers();
+    const response = await fetch(uri.users, form.postCreateUser(userName));
+    const user = await response.json();
+    $userList.prepend(createElementByString(template.user(user._id, user.name)));
 }
 
 const userCreateButton = document.querySelector('.user-create-button');
