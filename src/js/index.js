030688093df156521b05cf88c7e975667c58a42a
pika96 <52442949+pika96@users.noreply.github.com>
2021-04-06 02:50:26 +0900

    feat(index.js): User 추가하기 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,14 @@
 const onUserCreateHandler = () => {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  if(userName !== null) {
+    const todoList = document.getElementById("user-list");
+    todoList.insertAdjacentHTML("afterbegin", onAddUserItem(userName));
+  }
 }
 
 const userCreateButton = document.querySelector('.user-create-button')
 userCreateButton.addEventListener('click', onUserCreateHandler)
 
+function onAddUserItem(userName) {
+  return ` <button class="ripple">${userName}</button> `;
+}
