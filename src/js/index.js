1a74845447e8b1c8eeb9883cba296d1f737b9ce8
pika96 <52442949+pika96@users.noreply.github.com>
2021-04-06 04:11:47 +0900

    feat: 선택한 유저 검은색으로 표시

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
-const onUserCreateHandler = () => {
+
+const userCreateButton = document.querySelector('.user-create-button')
+userCreateButton.addEventListener('click', onUserCreateHandler)
+const $userItem = document.querySelector(".user-list");
+$userItem.addEventListener('click', selectUser);
+
+function onUserCreateHandler() {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
   if(userName !== null) {
     const todoList = document.getElementById("user-list");
@@ -6,9 +12,18 @@ const onUserCreateHandler = () => {
   }
 }
 
-const userCreateButton = document.querySelector('.user-create-button')
-userCreateButton.addEventListener('click', onUserCreateHandler)
-
 function onAddUserItem(userName) {
   return ` <button class="ripple">${userName}</button> `;
+}
+
+function active(user) {
+  const users = document.getElementsByClassName("active");
+  for (let i = 0; i < users.length; i++) {
+    users[i].classList.remove("active");
+  }
+  user.classList.add("active");
+}
+
+function selectUser(event) {
+  active(event.target.closest("button"));
 }
