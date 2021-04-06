4f47ce2eb3a7ee80c993978b854090fedd55c323
NewWisdom <wlgp2500@gmail.com>
2021-04-06 11:26:42 +0900

    feat(): user 삭제하고 업데이트하기 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+window.onload = setUpUserList;
 const url = "https://js-todo-list-9ca3a.df.r.appspot.com";
 const userList = document.getElementById('user-list');
 const onUserCreateHandler = () => {
@@ -5,43 +6,71 @@ const onUserCreateHandler = () => {
     addUser(userName);
 }
 
+async function setUpUserList() {
+    await fetch(url + '/api/users')
+        .then((response) => response.json())
+        .then((data) => addUserList(data))
+        .then((data) => {
+            const list = document.querySelector('.ripple')
+            list.classList.add("active")
+        });
+}
+
 const onUserDeleteHandler = (e) => {
     const selectedUser = document.querySelector('.active')
-    const wantDelete = alert(selectedUser.innerText + "을 삭제하시겠습니까?");
-    if (wantDelete) deleteUser(e.target.id);
+    const wantDelete = confirm(selectedUser.innerText + "을 삭제하시겠습니까?");
+    if (wantDelete) deleteUser(selectedUser);
 }
 
-function deleteUser(userId) {
-    fetch(url + '/api/users/' + userId, userDeleteOp)
-        .then((response) => console.log(response));
+function deleteUser(selectedUser) {
+    fetch(url + '/api/users/' + selectedUser.id, userDeleteOp)
+        .then((response) => updateUserList())
+        .catch((e) => console.log(e));
 }
 
-function addUser(userName) {
-    fetch(url + '/api/users', userPostOp)
-        .then((response) => updateUserList());
+async function addUser(userName) {
+    fetch(url + '/api/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName
+        })
+    }).then((response) => updateUserList());
 }
 
 
-function updateUserList() {
+async function updateUserList() {
     fetch(url + '/api/users')
         .then((response) => response.json())
+        .then((data) => clearUserList(data))
         .then((data) => addUserList(data));
 }
 
-function addUserList(userListData) {
+function clearUserList(data) {
+    const count = userList.children.length;
+    for (let i = 0; i < count -2; i++) {
+        userList.removeChild(userList.firstChild)
+    }
+    return data;
+}
+
+async function addUserList(userListData) {
     for (let i = 0; i < userListData.length; i++) {
         let userButton = document.createElement("button");
         userButton.className = "ripple";
         userButton.id = userListData[i]._id;
-        userButton.addEventListener("click", selectUser(this));
+        userButton.addEventListener("click", selectUser);
         userButton.innerText = userListData[i].name;
         userList.insertBefore(userButton, userList.children[userList.children.length - 2]);
     }
 }
 
-function selectUser(userButton) {
-    fetch(url + "/api/users" + userButton.id)
-        .then()
+function selectUser(e) {
+    const selectedUser = document.querySelector('.active')
+    selectedUser.classList.remove('active');
+    e.target.classList.add('active');
 }
 
 const userCreateButton = document.querySelector('.user-create-button')
@@ -52,14 +81,4 @@ userDeleteButton.addEventListener('click', onUserDeleteHandler)
 
 const userDeleteOp = {
     method: "DELETE"
-}
-
-const userPostOp = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-        name: userName
-    })
 }
