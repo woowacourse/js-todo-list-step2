7d2cb10b129e9f8ce0a0d37aa63a5c6dfa0e9791
NewWisdom <wlgp2500@gmail.com>
2021-04-06 09:43:19 +0900

    feat(): user 추가하기 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,42 @@
+const url = "https://js-todo-list-9ca3a.df.r.appspot.com";
+const userList = document.getElementById('user-list');
 const onUserCreateHandler = () => {
-  const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+    const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+    addUser(userName);
+}
+
+function addUser(userName) {
+    fetch(url + '/api/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName
+        }),
+    }).then((response) => updateUserList());
+}
+
+
+function updateUserList() {
+    fetch(url + '/api/users')
+        .then((response) => response.json())
+        .then((data) => addUserList(data));
+}
+
+function addUserList(userListData) {
+    for (let i = 0; i < userListData.length; i++) {
+        let userButton = document.createElement("button");
+        userButton.className = "ripple";
+        userButton.id = userListData[i]._id;
+        userButton.addEventListener("click", selectUser(this));
+        userButton.innerText = userListData[i].name;
+        userList.insertBefore(userButton, userList.children[userList.children.length - 2]);
+    }
+}
+
+function selectUser(userButton) {
 }
 
 const userCreateButton = document.querySelector('.user-create-button')
 userCreateButton.addEventListener('click', onUserCreateHandler)
-
