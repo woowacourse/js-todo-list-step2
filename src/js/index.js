8bec32d2db0b1f80750c57cf3902efd3237de4cf
daum7766 <daum7766@naver.com>
2021-04-09 00:40:38 +0900

    feat: 유저 todo리스트 가져오기

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 const API_URL = 'https://js-todo-list-9ca3a.df.r.appspot.com/api/';
 const USER_LIST = document.querySelector('#user-list');
+const TODO_LIST = document.querySelector('.todo-list');
 
 window.onload = () => {
   const userCreateButton = document.querySelector('.user-create-button')
@@ -61,9 +62,120 @@ function addUser(user) {
     userListItemButton.setAttribute('data-action', 'selectUser');
     userListItemButton.setAttribute('selectuser', 'click');
     userListItemButton.textContent = user['name'];
+    userListItemButton.addEventListener('click', userListItemButtonEvent)
     userListItem.appendChild(userListItemButton);
 
     USER_LIST.appendChild(userListItem);
+}
+
+function userListItemButtonEvent(event) {
+  clearButtonData();
+  clearTodoList();
+  event.target.parentNode.setAttribute('data-active', true);
+  event.target.classList.add("active");
+  callGetUserAPI(event);
+}
+
+function clearButtonData() {
+  for (const userItem of USER_LIST.children) {
+    userItem.setAttribute('data-active', false);
+    userItem.firstChild.classList.remove("active");
+  }
+}
+
+function clearTodoList() {
+  while (TODO_LIST.children.length !== 0) {
+    TODO_LIST.removeChild(TODO_LIST.firstChild);
+  }
+}
+
+function callGetUserAPI(event) {
+  const userId = event.target.getAttribute('data-id');
+  fetch(API_URL+'users/'+userId)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('유저 정보 얻기 실패');
+    }
+    return response.json();
+  })
+  .then(addTodoList)
+  .catch((error => {
+    console.log(error)
+  }))
+}
+
+function addTodoList(response) {
+  console.log(response);
+  for (const todo of response['todoList']) {
+    
+    const todoListItem = document.createElement('todo-list-item');
+    todoListItem.setAttribute('key', todo['_id']);
+    todoListItem.setAttribute('data-_id', todo['_id']);
+    todoListItem.setAttribute('data-contents', todo['contents']);
+    todoListItem.setAttribute('data-contents', todo['isCompleted']);
+    todoListItem.setAttribute('data-priority', todo['priority']);
+
+    const todoListItemList = document.createElement('li');
+    todoListItemList.setAttribute('data-id', todo['_id']);
+
+    const todoListItemDiv = document.createElement('div');
+    todoListItemDiv.className = 'view';
+
+    const todoListItemInput = document.createElement('input');
+    todoListItemInput.className = 'toggle';
+    todoListItemInput.type = 'checkbox';
+    todoListItemInput.setAttribute('data-action', 'toggleTodo');
+    todoListItemInput.setAttribute('toggletodo', 'click');
+
+    const label = document.createElement('label');
+    label.className = 'label';
+    label.setAttribute('data-action', 'toggleEditingTodo');
+    label.setAttribute('toggleeditingtodo', 'dbclick');
+    label.textContent = todo['contents'];
+
+    if (todo['priority'] === 'NONE') {
+      const todoListItemSelect = document.createElement('select');
+      todoListItemSelect.className = 'chip select';
+      todoListItemSelect.setAttribute('data-action', 'selectPriority');
+      todoListItemSelect.setAttribute('selectpriority', 'change');
+
+      const noneOption = document.createElement('option');
+      noneOption.value = 'NONE';
+      noneOption.textContent = '순위';
+      noneOption.toggleAttribute('select');
+
+      const firstOption = document.createElement('option');
+      firstOption.value = 'FIRST';
+      firstOption.textContent = '1순위';
+
+      const secondOption = document.createElement('option');
+      secondOption.value = 'SECOND';
+      secondOption.textContent = '2순위';
+
+      todoListItemSelect.appendChild(noneOption);
+      todoListItemSelect.appendChild(firstOption);
+      todoListItemSelect.appendChild(secondOption);
+
+      label.prepend(todoListItemSelect);
+    } else {
+      const todoListItemSpan = document.createElement('span');
+      todoListItemSpan.className = 'chip';
+      if (todo['priority'] === 'FIRST') {
+        todoListItemSpan.classList.add('primary')
+        todoListItemSpan.textContent = '1순위';
+      } else {
+        todoListItemSpan.classList.add('secondary')
+        todoListItemSpan.textContent = '2순위';
+      }
+      label.prepend(todoListItemSpan);
+    }
+
+    todoListItemDiv.appendChild(todoListItemInput);
+    todoListItemDiv.appendChild(label);
+    todoListItemList.appendChild(todoListItemDiv);
+    todoListItem.appendChild(todoListItemList);
+    TODO_LIST.appendChild(todoListItem);
+  }
 }
 
 function callAddUserAPI(userName) {
