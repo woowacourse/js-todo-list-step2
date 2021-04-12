7380be47d61208637bad45011043b9a83e4af676
y_woo :) <dusdn1702@naver.com>
2021-04-12 20:55:38 +0900

    feat: user 별 투두리스트 불러오는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -18,6 +18,77 @@ async function onUserCreateHandler() {
   }
 }
 
+async function getUsers() {
+  const userList = await fetch(baseURL + "/api/users", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json());
+  addUserContent(userList);
+}
+
+function addUserContent(userList) {
+  const userListContent = document.querySelector("#user-list");
+  for(let i = 0; i < userList.length; i++) {
+      const userButton = document.createElement("button");
+      userButton.id = userList[i]._id;
+      userButton.innerText = userList[i].name;
+      userButton.classList.add("ripple");
+      userButton.addEventListener('click', onUserHandler);
+      userListContent.prepend(userButton);
+  }
+}
+
+function onUserHandler(event) {
+  document.querySelector(".active").classList.toggle("active");
+  event.target.classList.toggle("active");
+  document.querySelector("strong").innerText = event.target.innerText;
+  getSelectedUserTodo(event.target.id);
+}
+
+async function getSelectedUserTodo(userId) {
+  await fetch(baseURL + '/api/users/' + userId, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+      .then(data => updateTodos(data));
+}
+
+function makeTodo(content) {
+  return `<li>
+              <div class="view">
+                <input class="toggle" type="checkbox" />
+                <label class="label">
+                  <select class="chip select">
+                    <option value="0" selected>순위</option>
+                    <option value="1">1순위</option>
+                    <option value="2">2순위</option>
+                  </select>
+                  ${content}
+                </label>
+                <button class="destroy"></button>
+              </div>
+              <input class="edit" value=${content} />
+            </li>`;
+}
+
+async function updateTodos(todo) {
+  const todoListContent = document.querySelector('.todo-list');
+  for(let i = 0; i < todo.todoList.length; i++) {
+    let todoItem = makeTodo(todo.todoList[i].contents);
+    todoListContent.insertAdjacentHTML('beforeend', todoItem);
+  }
+  setCount(todo.todoList.length);
+}
+
+function setCount(index) {
+  const listCount = document.getElementsByClassName("todo-count")[0].childNodes[1];
+  listCount.innerHTML = index;
+}
+
+getUsers();
 const userCreateButton = document.querySelector('.user-create-button')
 userCreateButton.addEventListener('click', onUserCreateHandler)
-
