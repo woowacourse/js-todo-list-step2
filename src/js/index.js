b58406198a9ab173823bdecd2449b508589ecb01
y_woo :) <dusdn1702@naver.com>
2021-04-12 17:33:17 +0900

    feat: user 추가 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,21 @@
-const onUserCreateHandler = () => {
+const baseURL = 'https://js-todo-list-9ca3a.df.r.appspot.com';
+
+async function onUserCreateHandler() {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  if (userName.length >= 2) {
+    const data = {
+      name: userName
+    }
+    await fetch(baseURL + "/api/users", {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json());
+  } else {
+    alert('이름은 2글자 이상이어야 합니다.');
+  }
 }
 
 const userCreateButton = document.querySelector('.user-create-button')
