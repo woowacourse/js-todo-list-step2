fa7272b0ceb19890981e190a17321e6594aaae2f
binghe819 <binghe819@naver.com>
2021-04-12 20:44:47 +0900

    feat: User 추가 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,28 @@
-const onUserCreateHandler = () => {
+const baseURL = "https://js-todo-list-9ca3a.df.r.appspot.com";
+
+const inputUserName = () => {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  if (userName.length < 2) {
+    alert("User의 이름은 최소 2글자 이상이어야 한다.");
+    return inputUserName();
+  }
+  return userName;
+}
+
+const onUserCreateHandler = () => {
+  const userName = inputUserName();
+  fetch(`${baseURL}/api/users`, {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({
+      'name': `${userName}`
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => alert(err));
 }
 
 const userCreateButton = document.querySelector('.user-create-button')
